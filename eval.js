90246634129f44219d6e7c2eda2ffa59afcc512e
Lennard Behrens <behrenle@posteo.de>
2019-11-30 15:42:03 +0100

    tensor sum evaluation

--- eval.js
+++ eval.js
@@ -1,4 +1,5 @@
 const Parser = require('./parser.js');
+const Tools = require('./tools.js');
 
 function eval(str) {
   var root = Parser.parse(str);
@@ -12,10 +13,21 @@ function evalNode(node) {
       return node.value;
       break;
 
+    case "list":
+      var list = [];
+      for (var i = 0; i < node.elements.length; i++) {
+        list.push(evalNode(node.elements[i]));
+      }
+      return list;
+
     case "sum":
       console.log(typeof evalNode(node.elements[0]));
       if (typeof evalNode(node.elements[0]) == "number") {
         return sumScalar(node);
+      } else if (evalNode(node.elements[0]) instanceof Array) {
+        return sumTensor(node);
+      } else {
+        throw "unsupported types";
       }
   }
 }
@@ -33,6 +45,15 @@ function sumScalar(node) {
     } else {
       throw "sum: incompatible types";
     }
+  }
+  return v;
+}
+
+function sumTensor(node) {
+  var dims = Tools.tensorDims(evalNode(node.elements[0]));
+  var v = Tools.zeroTensor(dims);
+  for (var i = 0; i < node.elements.length; i++) {
+    v = Tools.sumTensorPair(v, evalNode(node.elements[i]), "+", node.elements[i].sign);
   }
   return v;
 }
