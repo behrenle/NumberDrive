53f4824b262f363769c4453755c060b6c9071d2f
Lennard Behrens <behrenle@posteo.de>
2019-12-02 00:58:00 +0100

    numberDrive module

--- numberDrive.js
+++ numberDrive.js
@@ -1,15 +1,8 @@
 const Eval = require('./eval.js');
-//const str = "[[3,2,1],[3,2,1],[3,2,1],[3,2,1],[3,2,1]]*[[1,0,0],[0,1,0],[0,0,1]]";
-//const str = "2*[1,2,3]*[[1,0,0],[0,1,0],[0,0,1]]/2";
-const str = "x^2";
+const defaults = require('./defaults.js');
 
-var scope = {
-  x: 2,
-  f: function(x) {
-    return x*x;
-  }
+module.exports = {
+  eval: function(str) {
+    return Eval.eval(str, defaults);
+  },
 };
-
-console.log(str);
-console.log(Eval.eval(str, scope, true));
-//console.log(Eval.eval("2*2"));
