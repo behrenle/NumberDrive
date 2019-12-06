f51bbae7aab7b04548df949c11c6289c2fb5c324
Lennard Behrens <behrenle@posteo.de>
2019-12-06 04:06:17 +0100

    added binco, F and B method

--- defaults.js
+++ defaults.js
@@ -9,6 +9,39 @@ function roundUpFix(x, y, errY) {
   return false;
 }
 
+// method copied from binomirechner.htm by U. Kalina
+function nueberk(n, k) {
+  var erg = 0;
+  if (n < k) return -1;
+    if (k ==0) {
+      erg=1;
+    }
+  else {
+    erg =n /k;
+  }
+  while(k >1) {
+    n--;
+    k--;
+    erg=erg *n/k;
+  }
+  return Math.round(erg);
+}
+
+// method copied from binomirechner.htm by U. Kalina
+function B(p, n, k) {
+  return nueberk(n,k) * Math.pow(p, k) * Math.pow((1 - p), (n - k));
+}
+
+// method copied from binomirechner.htm by U. Kalina
+function F(p, n, k) {
+  var erg = 0;
+  var i;
+  for (i = 0; i <= k; i++) {
+    erg = erg + B(p, n, i);
+  }
+  return erg;
+}
+
 function sin(x) {
   if (roundUpFix(Math.abs(x % Math.PI), 0, epsilon)) {
     return 0;
@@ -75,4 +108,7 @@ module.exports = {
   exp: exp,
   solveLinear: Solve.solveLinearSystem,
   nSolve: Solve.numericSolve,
+  binco: nueberk,
+  B: B,
+  F: F,
 }
