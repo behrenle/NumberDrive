f99a332f2c9f9850c8aecb026487e149ce95a8cb
Lennard Behrens <behrenle@posteo.de>
2020-02-10 22:36:44 +0100

    quadratic interpolation

--- derive.js
+++ derive.js
@@ -18,6 +18,14 @@ function derive() {
   // * return result
 }
 
+function qInterpolate(x1, y1, x2, y2, x3, y3) {
+  var a, b, c;
+  a = (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1-y2)) / ((x1 - x2) * (x1 - x3) * (x3 - x2));
+  b = (Math.pow(x1, 2) * (y2 - y3) + Math.pow(x2, 2) * (y3 - y1) + Math.pow(x3, 2) * (y1 - y2)) / ((x1 - x2) * (x1 - x3) * (x2 - x3));
+  c = (Math.pow(x1, 2) * (x2 * y3 - x3 * y2) + x1 * (Math.pow(x3, 2) * y2 - Math.pow(x2, 2) * y3) + x2 * x3 * y1 * (x2 - x3)) / ((x1 - x2) * (x1 - x3) * (x2 - x3));
+  return {a: a, b: b, c: c};
+}
+
 module.exports = {
   derive: derive,
 };
