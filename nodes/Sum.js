90951eb3ec72a915649dd974a66a6611695abd94
Lennard Behrens <behrenle@posteo.de>
2020-02-13 01:21:03 +0100

    added variables support for sums

--- nodes/Sum.js
+++ nodes/Sum.js
@@ -1,5 +1,6 @@
 const AbstractNode = require("./AbstractNode");
 const Number = require("./Number");
+const Symbol = require("./Symbol");
 
 class Sum extends AbstractNode {
   constructor(sign, mulSign) {
@@ -9,12 +10,27 @@ class Sum extends AbstractNode {
 
   evaluate(scope) {
     var result = new Number(0);
+    var symbols = [];
     for (var element of this.elements) {
-      result = result.addNumber(element.evaluate(scope));
+      var value = element.evaluate(scope);
+      if (value instanceof Symbol) {
+        symbols.push(value);
+      } else {
+        result = result.addNumber(value);
+      }
     }
-    result.setMulSign(this.getMulSign());
-    result.applySign(this.getSign());
-    return result;
+    if (symbols.length == 0) {
+      result.setMulSign(this.getMulSign());
+      result.applySign(this.getSign());
+      return result;
+    } else {
+      var node = new Sum(this.getSign(), this.getMulSign());
+      node.push(result);
+      for (var symbol of symbols) {
+        node.push(symbol);
+      }
+      return node;
+    }
   }
 }
 
