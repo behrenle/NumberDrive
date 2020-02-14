6211626b809d3cd26b38f6492cb3495d89d0debc
Lennard Behrens <behrenle@posteo.de>
2020-02-14 20:05:00 +0100

    scope class

---
+++ scope/Scope.js
@@ -0,0 +1,17 @@
+class scope {
+  constructor(values) {
+    this.values = typeof values == "object" ? values : {};
+  }
+
+  exists(name) {
+    return this.values[name] ? true : false;
+  }
+
+  getValue(name) {
+    return this.values[name];
+  }
+
+  setValue(name, value) {
+    this.values[name] = name;
+  }
+}
