7a9938d9f903e980bd93f4b1fabcb34461fe5e4e
Lennard Behrens <behrenle@posteo.de>
2020-02-14 02:30:46 +0100

    removed comment

--- nodes/Power.js
+++ nodes/Power.js
@@ -15,18 +15,6 @@ class Power extends AbstractContainer {
       return base.power(exp);
     }
   }
-    /*var result;
-    if (base instanceof Number && exp instanceof Number) {
-      result = base.power(exp);
-    } else {
-      result = new Power();
-      result.push(base);
-      result.push(exp);
-    }
-    result.setMulSign(this.getMulSign());
-    result.applySign(this.getSign());
-    return result;
-  }*/
 
   getBase() {
     return this.getElement(0);
