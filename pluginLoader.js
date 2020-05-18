021aa4f429862563231c483f77eeb5573e970e95
Lennard Behrens <behrenle@posteo.de>
2020-05-18 15:16:23 +0200

    es6 plugin loader

--- pluginLoader.js
+++ pluginLoader.js
@@ -1,15 +1,25 @@
-/*module.exports = [
-  require("./plugins/core/constants"),
-  require("./plugins/core/trigonometry"),
-  require("./plugins/core/solve"),
-  require("./plugins/core/nsolve"),
-  require("./plugins/core/nderive"),
-  require("./plugins/core/nintegral"),
-  require("./plugins/core/tensor"),
-  require("./plugins/core/table"),
-  require("./plugins/core/misc"),
-  require("./plugins/core/switch"),
-  require("./plugins/core/erf"),
-];*/
+import core_constants from "./plugins/core/constants.js";
+import core_trigonometry from "./plugins/core/trigonometry.js";
+import core_solve from "./plugins/core/solve.js";
+import core_nsolve from "./plugins/core/nsolve.js";
+import core_nderive from "./plugins/core/nderive.js";
+import core_nintegral from "./plugins/core/nintegral.js";
+import core_tensor from "./plugins/core/tensor.js";
+import core_table from "./plugins/core/table.js";
+import core_misc from "./plugins/core/misc.js";
+import core_switch from "./plugins/core/switch.js";
+import core_erf from "./plugins/core/erf.js";
 
-export default [];
+export default [
+  core_constants,
+  core_trigonometry,
+  core_solve,
+  core_nsolve,
+  core_nderive,
+  core_nintegral,
+  core_tensor,
+  core_table,
+  core_misc,
+  core_switch,
+  core_erf
+];
