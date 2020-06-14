bef6ecbff23c52b1e3bc85b76ac938d79005f03f
Lennard Behrens <behrenle@posteo.de>
2020-06-14 13:25:25 +0200

    Fixed behrenle/Math-Explorer#25

--- plugins/core/switch.js
+++ plugins/core/switch.js
@@ -4,7 +4,7 @@ import manual from "./manual/switch.js";
 function switchFunc(parameters, stack) {
   let params = tools.checkParameters(parameters, ["number", "term", "term"]),
       switchParam = params[0].getDecimalValue(),
-      expression1 = params[1];
+      expression1 = params[1],
       expression2 = params[2];
 
   if (switchParam.gte(0))
