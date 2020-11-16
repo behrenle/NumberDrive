a3d5ce83decced8dd6517b46a44dfc57a411f4da
Lennard Behrens <behrenle@posteo.de>
2020-11-16 10:29:36 +0100

    snapshot

--- plugins/core/manual/constants.js
+++ plugins/core/manual/constants.js
@@ -5,21 +5,30 @@ export default {
                 en: "e",
                 de: "e",
             },
-            description: "Euler's number"
+            description: {
+                en: "Euler's number",
+                de: "Euler's number"
+            }
         },
         {
             synopsis: {
                 en: "pi",
                 de: "pi"
             },
-            description: "π is commonly defined as the ratio of a circle's circumference C to its diameter d"
+            description: {
+                en: "π is commonly defined as the ratio of a circle's circumference C to its diameter d",
+                de: "π is commonly defined as the ratio of a circle's circumference C to its diameter d"
+            }
         },
         {
             synopsis: {
                 en: "deg",
                 de: "deg"
             },
-            description: "deg is the conversion factor between degrees and radians."
+            description: {
+                en: "deg is the conversion factor between degrees and radians.",
+                de: "deg is the conversion factor between degrees and radians."
+            }
         }
     ],
     functions: []
