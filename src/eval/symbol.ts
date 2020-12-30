b5053cb89ed6dba8e97f3ccd0def590dd709730c
Lennard Behrens <behrenle@posteo.de>
2020-12-30 17:24:33 +0100

    apply symbol meta

--- src/eval/symbol.ts
+++ src/eval/symbol.ts
@@ -1,8 +1,11 @@
 import {Node, Stack, Symbol} from "../types";
 import {getItem} from "../utils/stack";
+import {evaluate} from "./index";
+import {applyMetaToNode} from "../utils/meta";
 
 const evalSymbol = (symbol: Symbol, stack: Stack): Node => {
-    return getItem(stack, symbol.name);
+    const item = getItem(stack, symbol.name);
+    return evaluate(applyMetaToNode(item, symbol.meta), stack);
 };
 
 export default evalSymbol;
