75cb0fd621d87aa3439c51d44f09456d5b95e450
Lennard Behrens <behrenle@posteo.de>
2021-01-18 22:36:31 +0100

    integrated parser

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export {evaluate} from "./eval/index";
+import {Stack} from "./types";
+import parse from "./parser/parse";
+import {evalNode} from "./eval";
+
+export const evaluate = (input: string, stack: Stack) => {
+    return evalNode(parse(input), stack);
+}
+
--- src/parser/parse.ts
+++ src/parser/parse.ts
@@ -1,13 +1,6 @@
 import {parse} from "./parser";
 import {Node} from "../types";
 
-type Result = Node | string;
-
-export default (input: string): Result => {
-    try {
-        return parse(input) as Node;
-    } catch (e) {
-        return e.message;
-    }
+export default (input: string) => {
+    return parse(input) as Node;
 }
-
