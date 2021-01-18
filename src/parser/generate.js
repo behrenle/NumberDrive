67ffb84f212e34cc3155470a7fe964c846323403
Lennard Behrens <behrenle@posteo.de>
2021-01-18 21:59:59 +0100

    build parser from grammer

--- src/parser/generate.js
+++ src/parser/generate.js
@@ -1,19 +1,11 @@
 import pegjs from "pegjs";
-import tspegjs from "ts-pegjs";
 import fs from "fs";
-import path from "path";
 
-const grammarSource = fs.readFileSync(path.resolve(__dirname, 'grammar.peg'), 'utf8');
-
+const currentDir = "src/parser/"
+const grammarSource = fs.readFileSync(currentDir + 'grammar.peg', 'utf8');
 const parserSource = pegjs.generate(grammarSource, {
     output: "source",
     format: "commonjs",
-    plugins: [tspegjs],
-    "tspegjs": {
-        "noTslint": false,
-        "tslintIgnores": "rule1,rule2",
-        "customHeader": "// import lib\nimport { Lib } from 'mylib';"
-    }
 });
 
-fs.writeFileSync(path.resolve(__dirname, 'parser.ts'), parserSource);
+fs.writeFileSync(currentDir + 'parser.js', parserSource);
